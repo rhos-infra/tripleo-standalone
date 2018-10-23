redhat-openstack/infrared

Infrared tripleo standalone plugin
Added a new plugin to setup openstack in standalone mode [1]
To deploy openstack in stnadalone mode, we need to have a
standalone host in the invnetory.
Currently only OSP is supported.

Main reason of adding new plugin is to not pollute existing plugins
with new extra conditions etc.

[1] https://blueprints.launchpad.net/tripleo/+spec/all-in-one
[2] https://bugzilla.redhat.com/show_bug.cgi?id=1577236

Change-Id: I692062f4ba4964996f23c6bbc55d1e117bcc15a3

plugins/tripleo-standalone/plugin.spec
@@ -1,0 +1,118 @@
+---
+config:
+    plugin_type: install
+subparsers:
+    tripleo-standalone:
+        description: The standalone triploe installation of the openstack
+        include_groups: ["Ansible options", "Inventory", "Common options", "Answers file"]
+        groups:
+            - title: Deploy parameters
+              options:
+                  stackname:
+                      type: Value
+                      help: Overcloud stack Name
+                      default: standalone
+                  local-interface:
+                      type: Value
+                      help: The data network interface name
+                      default: eth0
+            - title: Setup Packages
+              options:
+                  mirror:
+                      type: Value
+                      help: |
+                          Enable usage of specified mirror (for rpm, pip etc) [brq,qeos,tlv - or hostname].
+                          (Specified mirror needs to proxy multiple rpm source hosts and pypi packages.)
+
+                  version:
+                      type: Value
+                      required: yes
+                      help: |
+                          The product version (product == director)
+                          Numbers are for OSP releases
+                          Names are for RDO releases
+                      choices:
+                        - "7"
+                        - "8"
+                        - "9"
+                        - "10"
+                        - "11"
+                        - "12"
+                        - "13"
+                        - "14"
+                        - kilo
+                        - liberty
+                        - mitaka
+                        - newton
+                        - ocata
+                        - pike
+                        - queens
+                        - rocky
+                  build:
+                      help: |
+                          String represents a timestamp of the OSP puddle.
+                          Note: for versions 6 < OSPd < 10 to specify director
+                          version use '--director-build' flag.
+                          (for the given product core version).
+                          Supports any rhos-release labels.
+                          RDO supported labels: master-tripleo-ci
+                          Examples: "passed_phase1", "2016-08-11.1", "Y1", "Z3", "GA"
+                      type: Value
+
+                  director-build:
+                      help: |
+                          String represents a timestamp of the OSP director puddle
+                          (for the given product core version). Only applies for
+                          6 < OSPd < 10, and could be used with '--build' flag.
+                          Note: for versions >= 10 only the --build flag should be used to
+                          specify a puddle.
+                          Supports any rhos-release labels.
+                          Examples: "passed_phase1", "2016-08-11.1", "Y1", "Z3", "GA"
+                          If missing, will equal to "latest".
+                      type: Value
+
+                  buildmods:
+                      type: Value
+                      help: |
+                          List of flags for rhos-release module.
+                          Currently works with
+                          pin - pin puddle (dereference 'latest' links to prevent content from changing)
+                          flea - enable flea repos
+                          unstable - this will enable brew repos or poodles (in old releases)
+                          cdn - use internal mirrors of the CDN repos. (internal use)
+                          none - use none of those flags
+                      default: pin
+
+            - title: Custom Repositories
+              options:
+                  cdn:
+                      type: FileValue
+                      help: |
+                          YAML file
+                          Register the undercloud with a Red Hat Subscription Management platform.
+                          see documentation for more details
+                  repos-config:
+                      type: VarFile
+                      help: |
+                          YAML file
+                          define new repositories or update existing according to file.
+                          see documentation for more details
+                  repos-urls:
+                      type: Value
+                      help: |
+                          comma separated list of URLs to download repo files to ``/etc/yum.repos.d``
+                  repos-skip-release:
+                      type: Bool
+                      help: |
+                          specifies whether the rhos/rdo-release tools should
+                          be used to install tripleo packages. This flag also disables installation of the extra cdn
+                          repositories.
+                  skip-remove-repo:
+                      type: Value
+                      action: append
+                      help: |
+                          In the rhos-release role all repositories are clean up before tripleo-undercloud
+                          deployment, if some repos are required during deployment then use option
+                          '--skip-remove-repo' and specify path to the repository file. In that case this
+                          repository will be ignored during delete procedure.
+                          Example: --skip-remove-repo /etc/yum.repos.d/rhel-updates.repo

rhos-infra/tripleo-standalone

plugin.spec
@@ -1,0 +1,118 @@
+---
+config:
+    plugin_type: install
+subparsers:
+    tripleo-standalone:
+        description: The standalone tripleo installation of the openstack
+        include_groups: ["Ansible options", "Inventory", "Common options", "Answers file"]
+        groups:
+            - title: Deploy parameters
+              options:
+                  stackname:
+                      type: Value
+                      help: Overcloud stack Name
+                      default: standalone
+                  local-interface:
+                      type: Value
+                      help: The data network interface name
+                      default: eth0
+            - title: Setup Packages
+              options:
+                  mirror:
+                      type: Value
+                      help: |
+                          Enable usage of specified mirror (for rpm, pip etc) [brq,qeos,tlv - or hostname].
+                          (Specified mirror needs to proxy multiple rpm source hosts and pypi packages.)
+
+                  version:
+                      type: Value
+                      required: yes
+                      help: |
+                          The product version (product == director)
+                          Numbers are for OSP releases
+                          Names are for RDO releases
+                      choices:
+                        - "7"
+                        - "8"
+                        - "9"
+                        - "10"
+                        - "11"
+                        - "12"
+                        - "13"
+                        - "14"
+                        - kilo
+                        - liberty
+                        - mitaka
+                        - newton
+                        - ocata
+                        - pike
+                        - queens
+                        - rocky
+                  build:
+                      help: |
+                          String represents a timestamp of the OSP puddle.
+                          Note: for versions 6 < OSPd < 10 to specify director
+                          version use '--director-build' flag.
+                          (for the given product core version).
+                          Supports any rhos-release labels.
+                          RDO supported labels: master-tripleo-ci
+                          Examples: "passed_phase1", "2016-08-11.1", "Y1", "Z3", "GA"
+                      type: Value
+
+                  director-build:
+                      help: |
+                          String represents a timestamp of the OSP director puddle
+                          (for the given product core version). Only applies for
+                          6 < OSPd < 10, and could be used with '--build' flag.
+                          Note: for versions >= 10 only the --build flag should be used to
+                          specify a puddle.
+                          Supports any rhos-release labels.
+                          Examples: "passed_phase1", "2016-08-11.1", "Y1", "Z3", "GA"
+                          If missing, will equal to "latest".
+                      type: Value
+
+                  buildmods:
+                      type: Value
+                      help: |
+                          List of flags for rhos-release module.
+                          Currently works with
+                          pin - pin puddle (dereference 'latest' links to prevent content from changing)
+                          flea - enable flea repos
+                          unstable - this will enable brew repos or poodles (in old releases)
+                          cdn - use internal mirrors of the CDN repos. (internal use)
+                          none - use none of those flags
+                      default: pin
+
+            - title: Custom Repositories
+              options:
+                  cdn:
+                      type: FileValue
+                      help: |
+                          YAML file
+                          Register the undercloud with a Red Hat Subscription Management platform.
+                          see documentation for more details
+                  repos-config:
+                      type: VarFile
+                      help: |
+                          YAML file
+                          define new repositories or update existing according to file.
+                          see documentation for more details
+                  repos-urls:
+                      type: Value
+                      help: |
+                          comma separated list of URLs to download repo files to ``/etc/yum.repos.d``
+                  repos-skip-release:
+                      type: Bool
+                      help: |
+                          specifies whether the rhos/rdo-release tools should
+                          be used to install tripleo packages. This flag also disables installation of the extra cdn
+                          repositories.
+                  skip-remove-repo:
+                      type: Value
+                      action: append
+                      help: |
+                          In the rhos-release role all repositories are clean up before tripleo-undercloud
+                          deployment, if some repos are required during deployment then use option
+                          '--skip-remove-repo' and specify path to the repository file. In that case this
+                          repository will be ignored during delete procedure.
+                          Example: --skip-remove-repo /etc/yum.repos.d/rhel-updates.repo